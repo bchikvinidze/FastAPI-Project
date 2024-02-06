f377fbe506ff42c0d0bdd54dab0c9be5d4307d09
bubeshka <bubuchikvinidze@gmail.com>
2024-02-06 19:39:23 +0400

    fee test statistics_nonzero_fee

--- library/tests/test_statistics.py
+++ library/tests/test_statistics.py
@@ -1,9 +1,9 @@
 from starlette.testclient import TestClient
 
-from constants import ADMIN_API_KEY
+from constants import ADMIN_API_KEY, TRANSACTION_FEE
 
 
-def test_statistics(client: TestClient) -> None:
+def test_statistics_no_fee(client: TestClient) -> None:
     # create user and wallets
     response = client.post("/users")
     api_key = response.json()["user"]["key"]
@@ -41,6 +41,50 @@ def test_statistics(client: TestClient) -> None:
     assert statistics_response.status_code == 200
     assert transaction_number == 1
     assert platform_profit == 0
+
+
+def test_statistics_nonzero_fee(client: TestClient) -> None:
+    # create user and wallets
+    response = client.post("/users")
+    api_key = response.json()["user"]["key"]
+
+    # wallet
+    response = client.post("/wallets", headers={"x-api-key": api_key})
+    resp_wallet = response.json()["usd_wallet"]
+    wallet_from = resp_wallet["wallet_address"]
+
+    # create another user and wallet:
+    response = client.post("/users")
+    api_key2 = response.json()["user"]["key"]
+
+    # wallet
+    response = client.post("/wallets", headers={"x-api-key": api_key2})
+    resp_wallet = response.json()["usd_wallet"]
+    wallet_to = resp_wallet["wallet_address"]
+
+    # do transaction
+    send_amount = 0.7
+    transaction_response = client.post(
+        "/transactions",
+        headers={"x-api-key": api_key},
+        json={
+            "address_from": wallet_from,
+            "address_to": wallet_to,
+            "amount": send_amount,
+        },
+    )
+
+    statistics_response = client.get(
+        "/statistics", headers={"x-api-key": ADMIN_API_KEY}
+    )
+    transaction_number = statistics_response.json()["statistics"]["count_transactions"]
+    platform_profit = statistics_response.json()["statistics"]["total_profit"]
+
+    assert transaction_response.status_code == 201
+    assert transaction_response.json() == {}
+    assert statistics_response.status_code == 200
+    assert transaction_number == 1
+    assert platform_profit == TRANSACTION_FEE*send_amount
 
 
 def test_statistics_wrong_authentication(client: TestClient) -> None:
